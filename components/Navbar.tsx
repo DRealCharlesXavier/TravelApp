7f95173218e7ffbe6f8649e38169de846e1f5eb8
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-17 17:16:31 -1100

    Initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useState } from "react";
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
+import { menu, close, theplug } from "../public";
+
+const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src={theplug} alt="logo" width={74} height={29} />
+      </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+
+      <div className="lg:hidden flex flex-1 justify-end items-center">
+        <Image
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] inline-block object-contain cursor-pointer"
+          onClick={() => setToggle(!toggle)}
+        />
+
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+        >
+          <ul className="list-none flex justify-end items-start flex-col gap-5">
+            {NAV_LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={link.key}
+                className={`${
+                  active === link.label ? "text-black" : "text-gray-50"
+                } regular - 16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition - all hover:font-bold`}
+                onClick={() => {
+                  setToggle(!toggle);
+                  setActive(link.label);
+                }}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
